a7918413d8e5456149f902ad4d9188933e3173f1
Zack <zahariahoratau@gmail.com>
2023-11-19 01:18:28 +0100

    Created domain project custom Exceptions.
    - Created DuplicateEntryException
    - Created ObjectNotFoundException

---
+++ DistrictSales.Api.Domain/Exceptions/ObjectNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistrictSales.Api.Domain.Exceptions;
+
+public sealed class ObjectNotFoundException : Exception
+{
+    public ObjectNotFoundException(string? message) : base(message)
+    {
+    }
+}
