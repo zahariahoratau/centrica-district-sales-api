42859c5fb9c549d977329921b0f8484de197fcb3
Zack <zahariahoratau@gmail.com>
2023-11-19 01:08:06 +0100

    Created initial solution and projects.

---
+++ DistrictSales.Api/Program.cs
@@ -0,0 +1,40 @@
+using DistrictSales.Api.Middleware;
+using DistrictSales.Api.SqlServer;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Host.UseSerilog();
+
+builder.Services.AddSqlServer();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddDateOnlyTimeOnlyStringConverters();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options => options.AllowInputFormatterExceptionMessages = false);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => c.UseDateOnlyTimeOnlyStringConverters());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
