b3f6357f422f8b45d32e571896bd8d5b5ecb62db
Zack <zahariahoratau@gmail.com>
2023-11-19 01:24:35 +0100

    Created mapping between DTOs and domains in API layer.

---
+++ DistrictSales.Api/Mapping/DtoToDomainMapping.cs
@@ -0,0 +1,53 @@
+﻿using DistrictSales.Api.Domain.Models;
+using DistrictSales.Api.Dto.V1.Requests;
+
+namespace DistrictSales.Api.Mapping;
+
+public static class DtoToDomainMapping
+{
+    public static CreateSalesperson MapToCreateSalesperson(this CreateSalespersonRequestV1 salespersonResponse)
+    {
+        return new CreateSalesperson(
+            FirstName: salespersonResponse.FirstName,
+            LastName: salespersonResponse.LastName,
+            BirthDate: salespersonResponse.BirthDate,
+            HireDate: salespersonResponse.HireDate,
+            Email: salespersonResponse.Email,
+            PhoneNumber: salespersonResponse.PhoneNumber
+        );
+    }
+
+    public static UpdateSalesperson MapToUpdateSalesperson(this UpdateSalespersonRequestV1 salespersonResponse, Guid id)
+    {
+        return new UpdateSalesperson(
+            Id: id,
+            FirstName: salespersonResponse.FirstName,
+            LastName: salespersonResponse.LastName,
+            BirthDate: salespersonResponse.BirthDate,
+            HireDate: salespersonResponse.HireDate,
+            Email: salespersonResponse.Email,
+            PhoneNumber: salespersonResponse.PhoneNumber
+        );
+    }
+
+    public static CreateDistrict MapToCreateDistrict(this CreateDistrictRequestV1 createDistrictRequest)
+    {
+        return new CreateDistrict(
+            PrimarySalespersonId: createDistrictRequest.PrimarySalespersonId,
+            Name: createDistrictRequest.Name,
+            IsActive: createDistrictRequest.IsActive,
+            NumberOfStores: createDistrictRequest.NumberOfStores
+        );
+    }
+
+    public static UpdateDistrict MapToUpdateDistrict(this UpdateDistrictRequestV1 updateDistrictRequest, Guid id)
+    {
+        return new UpdateDistrict(
+            Id: id,
+            PrimarySalespersonId: updateDistrictRequest.PrimarySalespersonId,
+            Name: updateDistrictRequest.Name,
+            IsActive: updateDistrictRequest.IsActive,
+            NumberOfStores: updateDistrictRequest.NumberOfStores
+        );
+    }
+}
