fe0446f30078f6e236405a2e85d5c3e2c69444d3
Zack <zahariahoratau@gmail.com>
2023-11-19 01:32:34 +0100

    Created DateOnlyTypeHandler for custom Dapper handling of DateOnly types.

---
+++ DistrictSales.Api.SqlServer/TypeHandlers/DateOnlyTypeHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using Dapper;
+
+namespace DistrictSales.Api.SqlServer.TypeHandlers;
+
+public class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
+{
+    public override void SetValue(IDbDataParameter parameter, DateOnly value) => parameter.Value = value.ToString();
+
+    public override DateOnly Parse(object value) => DateOnly.FromDateTime((DateTime)value);
+}
