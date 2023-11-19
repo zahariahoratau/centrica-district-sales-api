6f77623b759767f9c2ab90c354962575545ea8c9
Zack <zahariahoratau@gmail.com>
2023-11-19 01:31:41 +0100

    Created SqlHelper static class for reading local SQL files into the code.

---
+++ DistrictSales.Api.SqlServer/Helpers/SqlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace DistrictSales.Api.SqlServer.Helpers;
+
+public static class SqlHelper
+{
+    public static string ReadSqlFile(string fileName)
+    {
+        string? executingAssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        if (executingAssemblyPath is null)
+            throw new InvalidOperationException("Could not get the executing assembly path.");
+
+        string filePath = Path.Combine(executingAssemblyPath, "Sql", fileName);
+
+        return File.ReadAllText(filePath);
+    }
+}
