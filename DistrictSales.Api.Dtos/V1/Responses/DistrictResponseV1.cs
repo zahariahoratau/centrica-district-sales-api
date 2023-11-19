b3a0bb30cf539667c3f4e87b156b1c4c3c1de3f1
Zack <zahariahoratau@gmail.com>
2023-11-19 01:23:15 +0100

    Created initial DTO responses.
    - DistrictResponseV1
    - SalespersonResponseV1

---
+++ DistrictSales.Api.Dtos/V1/Responses/DistrictResponseV1.cs
@@ -0,0 +1,31 @@
+﻿namespace DistrictSales.Api.Dto.V1.Responses;
+
+public class DistrictResponseV1
+{
+    public DistrictResponseV1(
+        Guid id,
+        string name,
+        DateTime createdAtUtc,
+        bool isActive,
+        short? numberOfStores,
+        SalespersonResponseV1 primarySalesperson,
+        SalespersonResponseV1[] secondarySalespeople
+    )
+    {
+        Id = id;
+        Name = name;
+        CreatedAtUtc = createdAtUtc;
+        IsActive = isActive;
+        NumberOfStores = numberOfStores;
+        PrimarySalesperson = primarySalesperson;
+        SecondarySalespeople = secondarySalespeople;
+    }
+
+    public Guid Id { get; }
+    public string Name { get; }
+    public DateTime CreatedAtUtc { get; }
+    public bool IsActive { get; }
+    public short? NumberOfStores { get; }
+    public SalespersonResponseV1 PrimarySalesperson { get; }
+    public SalespersonResponseV1[] SecondarySalespeople { get; }
+}
