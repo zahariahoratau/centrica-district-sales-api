a0ff732a86b598a82d6e4dd58a7df268039b09ae
Zack <zahariahoratau@gmail.com>
2023-11-19 01:22:08 +0100

    Created Dto project custom validation attributes.
    - Created DateOnlyNotInFutureAttribute
    - Created StringDanishPhoneNumberAttribute
    - Created ValidGuidAttribute

---
+++ DistrictSales.Api.Dtos/ValidationAttributes/StringDanishPhoneNumberAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace DistrictSales.Api.Dto.ValidationAttributes;
+
+public sealed class StringDanishPhoneNumberAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        // Automatically pass if value is null. RequiredAttribute should be used to assert a value is not empty.
+        if (value == null) return true;
+
+        if (value.GetType() != typeof(string)) return false;
+
+        string phoneNumber = (string)value;
+        return Regex.IsMatch(phoneNumber, @"\d{8}");
+    }
+}
---
+++ DistrictSales.Api.Dtos/ValidationAttributes/ValidGuidAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DistrictSales.Api.Dto.ValidationAttributes;
+
+internal class ValidGuidAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        // Automatically pass if value is null. RequiredAttribute should be used to assert a value is not empty.
+        if (value == null) return true;
+
+        if (Guid.TryParse(value.ToString(), out var guid) is not true) return false;
+
+        return guid != Guid.Empty;
+    }
+}
