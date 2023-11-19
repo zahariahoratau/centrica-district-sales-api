2a04b9e7afa45f12bfa46b225e9ab602a5967639
Zack <zahariahoratau@gmail.com>
2023-11-19 01:23:44 +0100

    Created initial DTO requests.
    - CreateDistrictRequestV1
    - CreateSalespersonRequestV1
    - UpdateDistrictRequestV1
    - UpdateSalespersonRequestV1

---
+++ DistrictSales.Api.Dtos/V1/Requests/CreateSalespersonRequestV1.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DistrictSales.Api.Dto.ValidationAttributes;
+
+namespace DistrictSales.Api.Dto.V1.Requests;
+
+public class CreateSalespersonRequestV1
+{
+    public CreateSalespersonRequestV1(
+        string firstName,
+        string lastName,
+        DateOnly birthDate,
+        DateOnly hireDate,
+        string email,
+        string? phoneNumber
+    )
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthDate = birthDate;
+        HireDate = hireDate;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    [StringLength(50, MinimumLength = 1, ErrorMessage = Constants.StringLengthErrorMessage)]
+    public string FirstName { get; }
+
+    [StringLength(50, MinimumLength = 1, ErrorMessage = Constants.StringLengthErrorMessage)]
+    public string LastName { get; }
+
+    [DateOnlyNotInFuture(ErrorMessage = Constants.DateOnlyNotInFutureErrorMessage)]
+    public DateOnly BirthDate { get; }
+
+    public DateOnly HireDate { get; }
+
+    [EmailAddress]
+    public string Email { get; }
+
+    [StringDanishPhoneNumber(ErrorMessage = Constants.StringDanishPhoneNumberErrorMessage)]
+    public string? PhoneNumber { get; }
+}
---
+++ DistrictSales.Api.Dtos/V1/Requests/UpdateDistrictRequestV1.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DistrictSales.Api.Dto.ValidationAttributes;
+
+namespace DistrictSales.Api.Dto.V1.Requests;
+
+public class UpdateDistrictRequestV1
+{
+    public UpdateDistrictRequestV1(
+        Guid? primarySalespersonId,
+        string? name,
+        bool? isActive,
+        short? numberOfStores
+    )
+    {
+        PrimarySalespersonId = primarySalespersonId;
+        Name = name;
+        IsActive = isActive;
+        NumberOfStores = numberOfStores;
+    }
+
+    [ValidGuid(ErrorMessage = Constants.InvalidGuidErrorMessage)]
+    public Guid? PrimarySalespersonId { get; }
+
+    [StringLength(50, MinimumLength = 1, ErrorMessage = Constants.StringLengthErrorMessage)]
+    public string? Name { get; }
+
+    public bool? IsActive { get; }
+
+    [Range(1, 32000, ErrorMessage = Constants.RangeErrorMessage)]
+    public short? NumberOfStores { get; }
+}
