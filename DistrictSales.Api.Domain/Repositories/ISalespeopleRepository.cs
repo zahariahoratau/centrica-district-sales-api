c698087d529394cfc0433a48105ace9e4a12b50a
Zack <zahariahoratau@gmail.com>
2023-11-19 01:19:05 +0100

    Created domain layer initial repositories interfaces.
    - IDistrictsRepository
    - ISalespeopleRepository

---
+++ DistrictSales.Api.Domain/Repositories/ISalespeopleRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+using DistrictSales.Api.Domain.Models;
+
+namespace DistrictSales.Api.Domain.Repositories;
+
+public interface ISalespeopleRepository
+{
+    Task<IImmutableList<Salesperson>> GetAllAsync(CancellationToken cancellationToken);
+    Task<IImmutableList<Salesperson>> GetAllSecondaryByDistrictIdAsync(Guid districtId, CancellationToken cancellationToken);
+    Task<Salesperson> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Salesperson> CreateAsync(CreateSalesperson createSalesperson, CancellationToken cancellationToken);
+    Task UpdateAsync(UpdateSalesperson updateSalesperson, CancellationToken cancellationToken);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
