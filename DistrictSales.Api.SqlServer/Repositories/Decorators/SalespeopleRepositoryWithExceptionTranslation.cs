e3434f3c3e36be9da49c77c3c1ccbf3f6de3191e
Zack <zahariahoratau@gmail.com>
2023-11-19 01:34:10 +0100

    Created SalespeopleRepository decorator:
    - SalespeopleRepositoryWithExceptionTranslation

---
+++ DistrictSales.Api.SqlServer/Repositories/Decorators/SalespeopleRepositoryWithExceptionTranslation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using DistrictSales.Api.Domain.Exceptions;
+using DistrictSales.Api.Domain.Models;
+using DistrictSales.Api.Domain.Repositories;
+
+namespace DistrictSales.Api.SqlServer.Repositories.Decorators;
+
+public class SalespeopleRepositoryWithExceptionTranslation : ISalespeopleRepository
+{
+    private readonly ISalespeopleRepository _decoratedSalespeopleRepository;
+
+    public SalespeopleRepositoryWithExceptionTranslation(ISalespeopleRepository decoratedSalespeopleRepository)
+    {
+        _decoratedSalespeopleRepository = decoratedSalespeopleRepository;
+    }
+
+    public async Task<IImmutableList<Salesperson>> GetAllAsync(CancellationToken cancellationToken) =>
+        await _decoratedSalespeopleRepository.GetAllAsync(cancellationToken);
+
+    public async Task<IImmutableList<Salesperson>> GetAllSecondaryByDistrictIdAsync(Guid districtId, CancellationToken cancellationToken) =>
+        await _decoratedSalespeopleRepository.GetAllSecondaryByDistrictIdAsync(districtId, cancellationToken);
+
+    public async Task<Salesperson> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await _decoratedSalespeopleRepository.GetByIdAsync(id, cancellationToken);
+        }
+        catch (InvalidOperationException exception)
+        {
+            throw new ObjectNotFoundException($"Salesperson with Id: {id} could not be found.");
+        }
+    }
+
+    public async Task<Salesperson> CreateAsync(CreateSalesperson createSalesperson, CancellationToken cancellationToken) =>
+        await _decoratedSalespeopleRepository.CreateAsync(createSalesperson, cancellationToken);
+
+    public async Task UpdateAsync(UpdateSalesperson updateSalesperson, CancellationToken cancellationToken) =>
+        await _decoratedSalespeopleRepository.UpdateAsync(updateSalesperson, cancellationToken);
+
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken) =>
+        await _decoratedSalespeopleRepository.DeleteAsync(id, cancellationToken);
+}
