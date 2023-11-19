5abc617f86243a605ba071c2639398aaaf12c5e0
Zack <zahariahoratau@gmail.com>
2023-11-19 01:16:01 +0100

    Created initial District class models.
    - Created District domain model
    - Created CreateDistrict model
    - Created UpdateDistrict model

---
+++ DistrictSales.Api.Domain/Models/District/UpdateDistrict.cs
@@ -0,0 +1,9 @@
+﻿namespace DistrictSales.Api.Domain.Models;
+
+public record UpdateDistrict(
+    Guid Id,
+    Guid? PrimarySalespersonId,
+    string? Name,
+    bool? IsActive,
+    short? NumberOfStores
+);
