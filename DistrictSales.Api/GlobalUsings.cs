6a653b0dea5c81921e690ea0bb8e3b155e5b0cf5
Zack <zahariahoratau@gmail.com>
2023-11-19 11:54:43 +0100

    Refactored GlobalUsings comment in API project.

--- DistrictSales.Api/GlobalUsings.cs
+++ DistrictSales.Api/GlobalUsings.cs
@@ -1,4 +1,4 @@
-﻿// Global usings for most used namespaces in the project
+﻿// Global usings for the most commonly used namespaces in the project
 global using DistrictSales.Api.Domain.Models;
 global using DistrictSales.Api.Domain.Repositories;
 global using DistrictSales.Api.Dto.V1.Requests;
