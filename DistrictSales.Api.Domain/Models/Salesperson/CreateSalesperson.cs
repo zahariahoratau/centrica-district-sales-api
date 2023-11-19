93d0d38b84cee60a4aad9f0a7499e9d4a8c19590
Zack <zahariahoratau@gmail.com>
2023-11-19 01:17:44 +0100

    Created initial Salesperson class models.
    - Created Salesperson domain model
    - Created CreateSalesperson model
    - Created UpdateSalesperson model

---
+++ DistrictSales.Api.Domain/Models/Salesperson/CreateSalesperson.cs
@@ -0,0 +1,10 @@
+﻿namespace DistrictSales.Api.Domain.Models;
+
+public record CreateSalesperson(
+    string FirstName,
+    string LastName,
+    DateOnly BirthDate,
+    DateOnly HireDate,
+    string Email,
+    string? PhoneNumber
+);
