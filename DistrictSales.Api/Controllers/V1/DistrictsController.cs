bd6f7ab6730ebaa6836418ee5853c0981c5aed88
Zack <zahariahoratau@gmail.com>
2023-11-20 03:09:03 +0100

    Created documentation for the controller endpoints.

--- DistrictSales.Api/Controllers/V1/DistrictsController.cs
+++ DistrictSales.Api/Controllers/V1/DistrictsController.cs
@@ -15,6 +15,15 @@ public class DistrictsController : ControllerBase
         _districtsRepository = districtsRepository;
     }
 
+    /// <summary>
+    /// Gets all districts.
+    /// </summary>
+    /// <remarks>
+    /// The response includes the primary salesperson and all secondary salespeople for each district.
+    /// </remarks>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">Returns all districts as an array of DistrictResponseV1.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DistrictResponseV1[]))]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -27,6 +36,17 @@ public class DistrictsController : ControllerBase
             .ToArray();
     }
 
+    /// <summary>
+    /// Gets a district by id.
+    /// </summary>
+    /// <remarks>
+    /// The response includes the primary salesperson and all secondary salespeople.
+    /// </remarks>
+    /// <param name="id">The GUID of the district to get.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">Returns the district as a DistrictResponseV1.</response>
+    /// <response code="404">The district with the specified id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpGet("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DistrictResponseV1))]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -38,6 +58,15 @@ public class DistrictsController : ControllerBase
         return district.MapToDistrictResponseV1();
     }
 
+    /// <summary>
+    /// Creates a new district.
+    /// </summary>
+    /// <param name="createDistrictRequest">The district to create, as a CreateDistrictRequestV1.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="201">Returns the created district as a DistrictResponseV1.</response>
+    /// <response code="400">The district to create is invalid.</response>
+    /// <response code="404">The salesperson with the specified id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(DistrictResponseV1))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -54,6 +83,16 @@ public class DistrictsController : ControllerBase
         );
     }
 
+    /// <summary>
+    /// Updates a district.
+    /// </summary>
+    /// <param name="id">The GUID of the district to update.</param>
+    /// <param name="updateDistrictRequest">The district to update, as an UpdateDistrictRequestV1.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">Returns the updated district as a DistrictResponseV1.</response>
+    /// <response code="400">The update district object is invalid.</response>
+    /// <response code="404">The district with the specified id, or the salesperson id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpPut("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DistrictResponseV1))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -64,6 +103,14 @@ public class DistrictsController : ControllerBase
         await _districtsRepository.UpdateAsync(id, updateDistrictRequest.MapToUpdateDistrict(id), cancellationToken);
     }
 
+    /// <summary>
+    /// Deletes a district.
+    /// </summary>
+    /// <param name="id">The GUID of the district to delete.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">The district was deleted successfully.</response>
+    /// <response code="404">The district with the specified id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpDelete("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -73,6 +120,16 @@ public class DistrictsController : ControllerBase
         await _districtsRepository.DeleteAsync(id, cancellationToken);
     }
 
+    /// <summary>
+    /// Adds a secondary salesperson to a district.
+    /// </summary>
+    /// <param name="id">The GUID of the district to add the secondary salesperson to.</param>
+    /// <param name="salespersonId">The GUID of the secondary salesperson to add.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">The secondary salesperson was added successfully.</response>
+    /// <response code="400">The salesperson is already linked to the district.</response>
+    /// <response code="404">The district with the specified id, or the salesperson id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpPost("{id:guid}/secondary-salespeople/{salespersonId:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -83,6 +140,15 @@ public class DistrictsController : ControllerBase
         await _districtsRepository.AddSecondarySalespersonAsync(id, salespersonId, cancellationToken);
     }
 
+    /// <summary>
+    /// Removes a secondary salesperson from a district.
+    /// </summary>
+    /// <param name="id">The GUID of the district to remove the secondary salesperson from.</param>
+    /// <param name="salespersonId">The GUID of the secondary salesperson to remove.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">The secondary salesperson was removed successfully.</response>
+    /// <response code="404">The combination of district with the specified id and the salesperson id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpDelete("{id:guid}/secondary-salespeople/{salespersonId:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
--- DistrictSales.Api/Controllers/V1/SalespeopleController.cs
+++ DistrictSales.Api/Controllers/V1/SalespeopleController.cs
@@ -15,6 +15,12 @@ public class SalespeopleController : ControllerBase
         _salespeopleRepository = salespeopleRepository;
     }
 
+    /// <summary>
+    /// Gets all salespeople.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">Returns all salespeople as an array of SalespersonResponseV1.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SalespersonResponseV1[]))]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -27,6 +33,14 @@ public class SalespeopleController : ControllerBase
             .ToArray();
     }
 
+    /// <summary>
+    /// Gets a salesperson by id.
+    /// </summary>
+    /// <param name="id">The GUID of the salesperson to get.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">Returns the salesperson as a SalespersonResponseV1.</response>
+    /// <response code="404">The salesperson with the specified id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpGet("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SalespersonResponseV1))]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -38,6 +52,14 @@ public class SalespeopleController : ControllerBase
         return salesperson.MapToSalespersonResponseV1();
     }
 
+    /// <summary>
+    /// Creates a new salesperson.
+    /// </summary>
+    /// <param name="salespersonRequest">The salesperson to create, as a CreateSalespersonRequestV1.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="201">Returns the created salesperson as a SalespersonResponseV1.</response>
+    /// <response code="400">The salesperson to create is invalid.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(SalespersonResponseV1))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -53,8 +75,19 @@ public class SalespeopleController : ControllerBase
         );
     }
 
+    /// <summary>
+    /// Updates a salesperson.
+    /// </summary>
+    /// <param name="id">The GUID of the salesperson to update.</param>
+    /// <param name="salespersonRequest">The salesperson fields to update, as an UpdateSalespersonRequestV1.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">The salesperson was updated successfully.</response>
+    /// <response code="400">The update salesperson object is invalid.</response>
+    /// <response code="404">The salesperson with the specified id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpPut("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task PutAsync(Guid id, UpdateSalespersonRequestV1 salespersonRequest, CancellationToken cancellationToken = default)
@@ -62,6 +95,14 @@ public class SalespeopleController : ControllerBase
         await _salespeopleRepository.UpdateAsync(salespersonRequest.MapToUpdateSalesperson(id), cancellationToken);
     }
 
+    /// <summary>
+    /// Deletes a salesperson.
+    /// </summary>
+    /// <param name="id">The GUID of the salesperson to delete.</param>
+    /// <param name="cancellationToken">The cancellation token to be used.</param>
+    /// <response code="200">The salesperson was deleted successfully.</response>
+    /// <response code="404">The salesperson with the specified id was not found.</response>
+    /// <response code="500">An unexpected error occurred.</response>
     [HttpDelete("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
